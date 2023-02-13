ff54200312d103b2d925e9dadb43cf875452c522
felipebaum <Fe@980774537>
2023-02-13 16:11:49 -0300

    ok

--- convert.py
+++ convert.py
@@ -1,10 +1,11 @@
 import time
 import webbrowser
 import pyautogui
+
 lista = []
 f = open('mdfes.txt', 'r')
 for c in f:
     lista.append(c)
-    webbrowser.open_new_tab(c)
+    webbrowser.open(c)
     time.sleep(5)
-
+    pyautogui.press("ester")
