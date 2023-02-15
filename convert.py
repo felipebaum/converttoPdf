77af692859f3127c67a285e62a8d59ff0a34e80e
Baum <felipe_1100@hotmail.com>
2023-02-15 14:32:14 -0300

    ok

--- convert.py
+++ convert.py
@@ -3,9 +3,20 @@ import webbrowser
 import pyautogui
 
 lista = []
+contador=2483
 f = open('mdfes.txt', 'r')
 for c in f:
     lista.append(c)
     webbrowser.open(c)
+    time.sleep(45)
+    pyautogui.press("enter")
     time.sleep(5)
-    pyautogui.press("ester")
+    pyautogui.write(f"Mdfe{contador}")
+    pyautogui.press("enter")
+    contador+=1
+    pyautogui.keyDown('ctrl')
+    pyautogui.keyDown('w')
+    time.sleep(3)
+    pyautogui.keyUp('ctrl')
+    pyautogui.keyUp('w')
+
